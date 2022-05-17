af2f115e6a8febcf7ab378c65cbcc33965778036
Jakub Trllo <jakub.trllo@gmail.com>
2022-05-17 12:09:37 +0200

    added settings to client

---
+++ openpype/client/settings.py
@@ -0,0 +1,13 @@
+"""Not sure what will be availabe here.
+
+Project anatomy and project settings could be available from project entity,
+    if there is any.
+"""
+
+
+def get_anatomy_settings(project_name):
+    pass
+
+
+def get_project_settings(project_name):
+    pass
