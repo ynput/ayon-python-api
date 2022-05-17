2c19f9523b1cb13adeaed6d44b712e9c5eafa83b
Jakub Trllo <jakub.trllo@gmail.com>
2022-05-17 12:09:45 +0200

    added base of server info connection

---
+++ server_info/openpype_server_info/connection.py
@@ -0,0 +1,24 @@
+class OpenPypeServerConnection:
+    def __init__(self, server_url):
+        self._server_url = server_url
+
+    def connect(self):
+        pass
+
+    def get_current_version_production(self):
+        pass
+
+    def get_current_version_staging(self):
+        pass
+
+    def need_updates(self):
+        pass
+
+    def update(self):
+        pass
+
+    def get_addon_names(self):
+        pass
+
+    def get_addon_versions(self, addon_names):
+        pass
