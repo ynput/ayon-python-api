b1c2c9f66e8dddfbdeced0b563d05ead8db39733
Jakub Trllo <jakub.trllo@gmail.com>
2023-03-16 18:55:03 +0100

    fix new entity data creation

--- ayon_api/operations.py
+++ ayon_api/operations.py
@@ -1,7 +1,6 @@
 import copy
 import collections
 import uuid
-import datetime
 from abc import ABCMeta, abstractproperty
 
 import six
@@ -137,13 +136,20 @@ def new_version_entity(
     if data is None:
         data = {}
 
-    return {
+    output = {
         "id": _create_or_convert_to_id(entity_id),
         "version": int(version),
         "subsetId": _create_or_convert_to_id(subset_id),
         "attrib": attribs,
         "data": data
     }
+    if task_id:
+        output["taskId"] = task_id
+    if thumbnail_id:
+        output["thumbnailId"] = thumbnail_id
+    if author:
+        output["author"] = author
+    return output
 
 
 def new_hero_version_entity(
@@ -180,13 +186,20 @@ def new_hero_version_entity(
     if data is None:
         data = {}
 
-    return {
+    output = {
         "id": _create_or_convert_to_id(entity_id),
         "version": -abs(int(version)),
         "subsetId": subset_id,
         "attrib": attribs,
         "data": data
     }
+    if task_id:
+        output["taskId"] = task_id
+    if thumbnail_id:
+        output["thumbnailId"] = thumbnail_id
+    if author:
+        output["author"] = author
+    return output
 
 
 def new_representation_entity(
@@ -198,7 +211,6 @@ def new_representation_entity(
         name (str): Representation name considered as unique identifier
             of representation under version.
         version_id (str): Id of parent version.
-        context (Dict[str, Any]): Representation context used to fill template.
         attribs (Dict[str, Any]): Explicitly set attributes of representation.
         data (Dict[str, Any]): Representation entity data.
         entity_id (str): Predefined id of entity. New id is created
@@ -236,6 +248,8 @@ def new_workfile_info_doc(
         task_name (str): Task under which was workfile created.
         files (List[str]): List of rootless filepaths related to workfile.
         data (Dict[str, Any]): Additional metadata.
+        entity_id (str): Predefined id of entity. New id is created
+            if not passed.
 
     Returns:
         Dict[str, Any]: Skeleton of workfile info entity.
