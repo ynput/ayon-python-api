081d6f4d39e88c3418b904b1d98a9b36d49f5ffb
Tadeas Hejnic <hejnitad@fit.cvut.cz>
2023-04-25 14:34:41 +0200

    non_test: Helper function for better readability of tests.

--- tests/my_helper_functions.py
+++ tests/my_helper_functions.py
@@ -1,24 +1,35 @@
+"""Functions that are often used in tests."""
+
+import pytest
 from ayon_api._api import (
     get_folders,
     get_subsets,
     get_versions,
-    get_representations
+    get_representations,
+    get_folder_by_name
+)
+from ayon_api.operations import (
+    OperationsSession
 )
 
 
 PROJECT_NAME = "demo_Commercial"
 
 
-def my_get_folder_ids():
+def my_get_folder_ids(parent = None):
     folders = list(
         get_folders(
-            PROJECT_NAME
+            PROJECT_NAME,
+            parent_ids=[parent]
         )
     )
 
     return [folder["id"] for folder in folders]
 
 def my_get_subset_ids(folder_ids):
+    if not isinstance(folder_ids, list):
+        folder_ids = [folder_ids]
+
     subsets = list(
         get_subsets(
             PROJECT_NAME,
@@ -30,10 +41,13 @@ def my_get_subset_ids(folder_ids):
 
 
 def my_get_version_ids(subset_id):
+    if not isinstance(subset_id, list):
+        subset_ids = [subset_ids]
+
     versions = list(
         get_versions(
             PROJECT_NAME, 
-            subset_ids=[subset_id]
+            subset_ids=subset_ids
         )
     )
 
@@ -41,6 +55,9 @@ def my_get_version_ids(subset_id):
 
 
 def my_get_representation_ids(my_version_ids):
+    if not isinstance(my_version_ids, list):
+        my_version_ids = [my_version_ids]
+
     representations = list(
         get_representations(
             PROJECT_NAME, 
@@ -53,3 +70,24 @@ def my_get_representation_ids(my_version_ids):
 def my_delete_folder(s, name, id):
     s.delete_entity(name, "folder", id)
     s.commit()
+
+
+def manual_delete_hierarchy(folder_name, s=None):
+    if not s:
+        s = OperationsSession()
+
+    folder_id = str()
+    subset_ids = []
+    try:
+        folder_id = get_folder_by_name(PROJECT_NAME, folder_name)["id"]
+    except TypeError as exc:
+        print(exc)
+    else:
+        subset_ids = my_get_subset_ids([folder_id])
+
+        for subset_id in subset_ids:
+            s.delete_entity(PROJECT_NAME, "subset", subset_id)
+            s.commit()
+
+        s.delete_entity(PROJECT_NAME, "folder", folder_id)
+        s.commit()
