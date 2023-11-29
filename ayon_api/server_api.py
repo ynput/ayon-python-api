388ab8470044625116cacfae79a36b59d04d6626
Jakub Trllo <jakub.trllo@gmail.com>
2023-11-29 02:12:16 +0100

    use upload_file function to upload thumbnail

--- ayon_api/server_api.py
+++ ayon_api/server_api.py
@@ -1600,6 +1600,10 @@ class ServerAPI(object):
         download happens in thread and other thread want to catch changes over
         time.
 
+        Todos:
+            Use retries and timeout.
+            Return RestApiResponse.
+
         Args:
             endpoint (str): Endpoint or URL to file that should be downloaded.
             filepath (str): Path where file will be downloaded.
@@ -1665,7 +1669,13 @@ class ServerAPI(object):
             yield chunk
 
     def _upload_file(
-        self, url, filepath, progress, request_type=None, chunk_size=None
+        self,
+        url,
+        filepath,
+        progress,
+        request_type=None,
+        chunk_size=None,
+        **kwargs
     ):
         """
 
@@ -1678,6 +1688,8 @@ class ServerAPI(object):
                 be used. Default is PUT.
             chunk_size (Optional[int]): Size of chunks that are uploaded
                 at once.
+            **kwargs (Any): Additional arguments that will be passed
+                to request function.
 
         Returns:
             RestApiResponse: Server response.
@@ -1685,9 +1697,12 @@ class ServerAPI(object):
 
         if request_type is None:
             request_type = RequestTypes.put
-        kwargs = {}
+
         if self._session is None:
-            kwargs["headers"] = self.get_headers()
+            headers = kwargs.setdefault("headers", {})
+            for key, value in self.get_headers().items():
+                if key not in headers:
+                    headers[key] = value
             post_func = self._base_functions_mapping[request_type]
         else:
             post_func = self._session_functions_mapping[request_type]
@@ -1706,12 +1721,13 @@ class ServerAPI(object):
         return response
 
     def upload_file(
-        self, endpoint, filepath, progress=None, request_type=None
+        self, endpoint, filepath, progress=None, request_type=None, **kwargs
     ):
         """Upload file to server.
 
         Todos:
-            Uploading with more detailed progress.
+            Use retries and timeout.
+            Return RestApiResponse.
 
         Args:
             endpoint (str): Endpoint or url where file will be uploaded.
@@ -1720,6 +1736,8 @@ class ServerAPI(object):
                 to track upload progress.
             request_type (Optional[RequestType]): Type of request that will
                 be used to upload file.
+            **kwargs (Any): Additional arguments that will be passed
+                to request function.
 
         Returns:
             requests.Response: Response object.
@@ -1741,7 +1759,9 @@ class ServerAPI(object):
         progress.set_started()
 
         try:
-            return self._upload_file(url, filepath, progress, request_type)
+            return self._upload_file(
+                url, filepath, progress, request_type, **kwargs
+            )
 
         except Exception as exc:
             progress.set_failed(str(exc))
@@ -5574,16 +5594,14 @@ class ServerAPI(object):
             return thumbnail_id
 
         mime_type = self._get_thumbnail_mime_type(src_filepath)
-        with open(src_filepath, "rb") as stream:
-            content = stream.read()
-
-        response = self.raw_post(
+        response = self.upload_file(
             "projects/{}/thumbnails".format(project_name),
+            src_filepath,
+            request_type=RequestTypes.post,
             headers={"Content-Type": mime_type},
-            data=content
         )
         response.raise_for_status()
-        return response.data["id"]
+        return response.json()["id"]
 
     def update_thumbnail(self, project_name, thumbnail_id, src_filepath):
         """Change thumbnail content by id.
@@ -5604,13 +5622,11 @@ class ServerAPI(object):
             raise ValueError("Entered filepath does not exist.")
 
         mime_type = self._get_thumbnail_mime_type(src_filepath)
-        with open(src_filepath, "rb") as stream:
-            content = stream.read()
-
-        response = self.raw_put(
+        response = self.upload_file(
             "projects/{}/thumbnails/{}".format(project_name, thumbnail_id),
+            src_filepath,
+            request_type=RequestTypes.put,
             headers={"Content-Type": mime_type},
-            data=content
         )
         response.raise_for_status()
 
