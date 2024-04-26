8778eb1d4bbb4953d7961fbf25356d8539a5f40b
Jakub Trllo <43494761+iLLiCiTiT@users.noreply.github.com>
2024-04-26 14:38:34 +0200

    small changes in test server

--- tests/test_server.py
+++ tests/test_server.py
@@ -4,10 +4,8 @@ To run use: pytest --envfile {environment path}.
 Make sure you have set AYON_TOKEN in your environment. 
 """
 
-import json
 import os
 import pytest
-from dotenv import load_dotenv
 
 from ayon_api import (
     is_connection_created,
@@ -16,35 +14,17 @@ from ayon_api import (
     get_server_api_connection,
     get_base_url,
     get_rest_url,
-    get_tasks,
-    get_versions,
-    get_representation_by_id,
-    get_version_by_id,
-    get_folder_by_id,
-    get_product_by_id,
-    get_folder_by_name,
-    get_product_by_name,
-    get_representation_by_name,
-    get_folders,
-    get_products,
-    get_versions,
-    get_representations
-)
-from ayon_api.exceptions import (
-    FailedOperations
 )
 
-
-AYON_BASE_URL = os.getenv("AYON_SERVER_URL")
-AYON_REST_URL = "{}/api".format(AYON_BASE_URL )
 PROJECT_NAME = os.getenv("AYON_PROJECT_NAME")
-
+AYON_BASE_URL = os.getenv("AYON_SERVER_URL")
+AYON_REST_URL = "{}/api".format(AYON_BASE_URL)
 
 def test_close_connection():
     con = get_server_api_connection()
-    assert is_connection_created() == True
+    assert is_connection_created() is True
     close_connection()
-    assert is_connection_created() == False
+    assert is_connection_created() is False
 
 
 def test_get_base_url():
@@ -60,11 +40,11 @@ def test_get_rest_url():
 
 
 @pytest.mark.parametrize(
-    "folder, name",
+    "entity_type, name",
     [("projects", PROJECT_NAME)]
 )
-def test_get(folder, name):
-    entrypoint = folder + "/" + name
+def test_get(entity_type, name):
+    entrypoint = entity_type + "/" + name
     res = get(entrypoint)
     assert res.status_code == 200
     assert isinstance(res.data, dict)
