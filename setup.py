80309cd3b4f3f575b4f060bb99d04fe176b32576
Jakub Trllo <43494761+iLLiCiTiT@users.noreply.github.com>
2024-05-31 14:46:30 +0200

    removed python 2 classifiers

--- setup.py
+++ setup.py
@@ -26,7 +26,6 @@ setup(
     classifiers=[
         "Development Status :: 5 - Production/Stable",
         "Programming Language :: Python",
-        "Programming Language :: Python :: 2",
         "Programming Language :: Python :: 3",
     ],
     install_requires=[
