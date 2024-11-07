20dd3253db4ebae1f6e315117a7f6906a5533c33
Tadeas Hejnic <tadeas.hejnic@gmail.com>
2024-11-07 13:58:13 +0100

    get_events: New test for invalid names data, small code adjustments

--- tests/test_server.py
+++ tests/test_server.py
@@ -13,21 +13,20 @@ from ayon_api import (
     get,
     get_events,
     get_project_names,
-    get_user,
+    get_user_by_name,
     get_server_api_connection,
     get_base_url,
     get_rest_url,
     get_timeout,
     set_timeout
 )
-from ayon_api import exceptions
 
 AYON_BASE_URL = os.getenv("AYON_SERVER_URL")
 AYON_REST_URL = "{}/api".format(AYON_BASE_URL)
 
 
 def test_close_connection():
-    _con = get_server_api_connection()
+    _ = get_server_api_connection()
     assert is_connection_created() is True
     close_connection()
     assert is_connection_created() is False
@@ -100,7 +99,7 @@ test_include_logs = [
 test_has_children = [
     (None),
     (True),
-    (False),
+    # (False),
 ]
 
 from datetime import datetime, timedelta, timezone
@@ -156,9 +155,9 @@ def test_get_events_all_filter_combinations(
     # with many tests - ayon_api.exceptions.ServerError: Connection timed out.
     # TODO - maybe some better solution
     if get_timeout() < 5:
-        set_timeout(10.0)
+        set_timeout(20.0)
 
-    res = get_events(
+    res = list(get_events(
         topics=topics, 
         project_names=project_names,
         states=states,
@@ -168,12 +167,10 @@ def test_get_events_all_filter_combinations(
         newer_than=newer_than,
         older_than=older_than,
         fields=fields
-    )
-
-    list_res = list(res)
+    ))
 
     # test if filtering was correct
-    for item in list_res:
+    for item in res:
         assert item.get("topic") in topics, (
             f"Expected 'project' one of values: {topics}, but got '{item.get('topic')}'"
         )
@@ -194,7 +191,7 @@ def test_get_events_all_filter_combinations(
         )
 
     # test if all events were given
-    assert topics is None or len(list_res) == sum(len(list(get_events(
+    assert topics is None or len(res) == sum(len(list(get_events(
         topics=[topic], 
         project_names=project_names,
         states=states,
@@ -206,7 +203,7 @@ def test_get_events_all_filter_combinations(
         fields=fields)
     )) for topic in topics)
 
-    assert project_names is None or len(list_res) == sum(len(list(get_events(
+    assert project_names is None or len(res) == sum(len(list(get_events(
         topics=topics, 
         project_names=[project_name],
         states=states,
@@ -218,7 +215,7 @@ def test_get_events_all_filter_combinations(
         fields=fields)
     )) for project_name in project_names)
     
-    assert states is None or  len(list_res) == sum(len(list(get_events(
+    assert states is None or  len(res) == sum(len(list(get_events(
         topics=topics, 
         project_names=project_names,
         states=[state],
@@ -230,7 +227,7 @@ def test_get_events_all_filter_combinations(
         fields=fields)
     )) for state in states)
     
-    assert users is None or len(list_res) == sum(len(list(get_events(
+    assert users is None or len(res) == sum(len(list(get_events(
         topics=topics, 
         project_names=project_names,
         states=states,
@@ -242,7 +239,7 @@ def test_get_events_all_filter_combinations(
         fields=fields)
     )) for user in users)
 
-    assert fields is None or len(list_res) == sum(len(list(get_events(
+    assert fields is None or len(res) == sum(len(list(get_events(
         topics=topics, 
         project_names=project_names,
         states=states,
@@ -257,67 +254,152 @@ def test_get_events_all_filter_combinations(
 
 @pytest.mark.parametrize("project_names", test_project_names)
 def test_get_events_project_name(project_names):
-    res = get_events(project_names=project_names)
-
-    list_res = list(res)
-
-    users = set()
-    for item in list_res:
-        users.add(item.get("user"))
+    res = list(get_events(project_names=project_names))
+    
+    for item in res:
         assert item.get("project") in project_names, f"Expected 'project' value '{project_names}', but got '{item.get('project')}'"
 
     # test if the legths are equal
-    assert len(list_res) == sum(len(list(get_events(project_names=[project_name]))) for project_name in project_names)
+    assert len(res) == sum(len(list(get_events(project_names=[project_name]))) for project_name in project_names)
 
 
 @pytest.mark.parametrize("project_names", test_project_names)
 @pytest.mark.parametrize("topics", test_topics)
 def test_get_events_project_name_topic(project_names, topics):
     print(project_names, "", topics)
-    res = get_events(topics=topics, project_names=project_names)
+    res = list(get_events(
+        topics=topics,
+        project_names=project_names
+    ))
 
-    list_res = list(res)
-
-    for item in list_res:
+    for item in res:
         assert item.get("topic") in topics
         assert item.get("project") in project_names, f"Expected 'project' value '{project_names}', but got '{item.get('project')}'"
     
     # test if the legths are equal
-    assert len(list_res) == sum(len(list(get_events(project_names=[project_name], topics=topics))) for project_name in project_names)
-    assert len(list_res) == sum(len(list(get_events(project_names=project_names, topics=[topic]))) for topic in topics)
+    assert len(res) == sum(len(list(get_events(project_names=[project_name], topics=topics))) for project_name in project_names)
+    assert len(res) == sum(len(list(get_events(project_names=project_names, topics=[topic]))) for topic in topics)
 
 
 @pytest.mark.parametrize("project_names", test_project_names)
 @pytest.mark.parametrize("topics", test_topics)
 @pytest.mark.parametrize("users", test_users)
 def test_get_events_project_name_topic_user(project_names, topics, users):
-    # print(project_names, "", topics)
-    res = get_events(topics=topics, project_names=project_names, users=users)
+    res = list(get_events(
+        topics=topics,
+        project_names=project_names,
+        users=users
+    ))
 
-    list_res = list(res)
-
-    for item in list_res:
+    for item in res:
         assert item.get("topic") in topics, f"Expected 'project' one of values: {topics}, but got '{item.get('topic')}'"
         assert item.get("project") in project_names, f"Expected 'project' one of values: {project_names}, but got '{item.get('project')}'"
         assert item.get("user") in project_names, f"Expected 'project' one of values: {users}, but got '{item.get('user')}'"
 
     # test if the legths are equal
-    assert len(list_res) == sum(len(list(get_events(project_names=[project_name], topics=topics))) for project_name in project_names)
-    assert len(list_res) == sum(len(list(get_events(project_names=project_names, topics=[topic]))) for topic in topics)
-    assert len(list_res) == sum(len(list(get_events(project_names=project_names, topics=[topic]))) for topic in topics)
+    assert len(res) == sum(len(list(get_events(project_names=[project_name], topics=topics))) for project_name in project_names)
+    assert len(res) == sum(len(list(get_events(project_names=project_names, topics=[topic]))) for topic in topics)
+    assert len(res) == sum(len(list(get_events(project_names=project_names, topics=[topic]))) for topic in topics)
 
 
 @pytest.mark.parametrize("newer_than", test_newer_than)
 @pytest.mark.parametrize("older_than", test_older_than)
 def test_get_events_timestamp(newer_than, older_than):
-    res = get_events(newer_than=newer_than, older_than=older_than)
+    res = list(get_events(
+        newer_than=newer_than,
+        older_than=older_than
+    ))
 
-    list_res = list(res)
-
-    for item in list_res:
+    for item in res:
         assert (newer_than is None) or (
             datetime.fromisoformat(item.get("createdAt") > datetime.fromisoformat(newer_than))
         )
         assert (older_than is None) or (
             datetime.fromisoformat(item.get("createdAt") < datetime.fromisoformat(older_than))
         )
+
+
+test_invalid_topics = [
+    (None),
+    (["invalid_topic_name_1", "invalid_topic_name_2"]),
+    (["invalid_topic_name_1"]),
+]
+
+test_invalid_project_names = [
+    (None),
+    (["invalid_project"]),
+    (["invalid_project", "demo_Big_Episodic", "demo_Big_Feature"]),
+    (["invalid_name_2", "demo_Commercial"]),
+    (["demo_Commercial"]),
+]
+
+test_invalid_states = [
+    (None),
+    (["pending_invalid"]),
+    (["in_progress_invalid"]), 
+    (["finished_invalid", "failed_invalid"]),
+]
+
+test_invalid_users = [
+    (None),
+    (["ayon_invalid_user"]),
+    (["ayon_invalid_user1", "ayon_invalid_user2"]),
+    (["ayon_invalid_user1", "ayon_invalid_user2", "admin"]),
+]
+
+test_invalid_newer_than = [
+    (None),
+    ((datetime.now(timezone.utc) + timedelta(days=2)).isoformat()),
+    ((datetime.now(timezone.utc) + timedelta(days=5)).isoformat()),
+    ((datetime.now(timezone.utc) - timedelta(days=5)).isoformat()),
+]
+
+
+@pytest.mark.parametrize("topics", test_invalid_topics)
+@pytest.mark.parametrize("project_names", test_invalid_project_names)
+@pytest.mark.parametrize("states", test_invalid_states)
+@pytest.mark.parametrize("users", test_invalid_users)
+@pytest.mark.parametrize("newer_than", test_invalid_newer_than)
+def test_get_events_invalid_data(
+    topics, 
+    project_names,
+    states,
+    users,
+    newer_than
+):
+    # with many tests - ayon_api.exceptions.ServerError: Connection timed out.
+    # TODO - maybe some better solution
+    if get_timeout() < 5:
+        set_timeout(20.0)
+
+    res = list(get_events(
+        topics=topics, 
+        project_names=project_names,
+        states=states,
+        users=users,
+        newer_than=newer_than
+    ))
+
+    valid_project_names = get_project_names()
+
+    assert res == [] \
+        or topics is None 
+    assert res == [] \
+        or project_names is None \
+        or any(project_name in valid_project_names for project_name in project_names)
+    assert res == [] \
+        or states is None
+    assert res == [] \
+        or users is None \
+        or any(get_user_by_name(user) is not None for user in users)
+    assert res == [] \
+        or newer_than is None \
+        or datetime.fromisoformat(newer_than) < datetime.now(timezone.utc)
+
+
+test_update_sender = [
+    (),
+]
+
+# def test_update_event():
+
