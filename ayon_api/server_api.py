2c266e0e24c4dca9053d41a6b94565718bdcc32c
Jakub Trllo <43494761+iLLiCiTiT@users.noreply.github.com>
2024-11-25 18:55:16 +0100

    raise an error if ignore sender types are not supported

--- ayon_api/server_api.py
+++ ayon_api/server_api.py
@@ -1761,9 +1761,13 @@ class ServerAPI(object):
             kwargs["ignoreOlderThan"] = ignore_older_than
         if (
             ignore_sender_types is not None
-            and (major, minor, patch) > (1, 5, 4)
         ):
-            kwargs["ignoreSenderTypes"] = ignore_sender_types
+            if (major, minor, patch) > (1, 5, 4):
+                raise ValueError(
+                    "Ignore sender types are not supported for"
+                    f" your version of server {self.server_version}."
+                )
+            kwargs["ignoreSenderTypes"] = list(ignore_sender_types)
 
         response = self.post("enroll", **kwargs)
         if response.status_code == 204:
