f431d4cf5f10ea43710199a1d7e555455a4271fe
Jakub Trllo <43494761+iLLiCiTiT@users.noreply.github.com>
2025-04-14 13:22:48 +0200

    fix endpoint for task method

--- ayon_api/server_api.py
+++ ayon_api/server_api.py
@@ -7899,7 +7899,7 @@ class ServerAPI(object):
                 valid.
 
         """
-        return self.get_thumbnail(project_name, "folder", task_id)
+        return self.get_thumbnail(project_name, "task", task_id)
 
     def get_version_thumbnail(
         self,
