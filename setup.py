d0487ce2902e1ef4077c57c722a87013de18f8da
Jakub Trllo <43494761+iLLiCiTiT@users.noreply.github.com>
2026-08-21 12:47:08 +0200

    add websocket client to requirements

--- setup.py
+++ setup.py
@@ -31,6 +31,7 @@ setup(
     install_requires=[
         "requests >= 2.27.1",
         "Unidecode >= 1.3.0",
+        "websocket-client >= 1.7.0",
         "appdirs >=1, <2",
     ],
     keywords=["AYON", "ynput", "OpenPype", "vfx"],
